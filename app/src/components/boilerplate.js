db4ccaa11755bc482ffe829869c41e0853fdd53e
Jason Smith <jasmith@najasmith.local>
2015-12-21 12:33:48 -0500

    Adding eslint and some testing helpers

---
+++ app/src/components/boilerplate.js
@@ -0,0 +1,7 @@
+export default function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Wololo wololo!';
+
+    return element;
+};
---
+++ app/src/index.js
@@ -0,0 +1,8 @@
+import component from './components/boilerplate';
+
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
+
